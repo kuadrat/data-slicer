c635488d4508808f6283ae01823558598cf895f1
Kevin Kramer <kevinpkramer@gmail.com>
2021-06-03 10:21:08 +0200

    new logo v02

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -60,7 +60,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "../../img/Data_Slicer_RZ.png"
+html_logo = "../../img/02_Data_Slicer_Logo.png"
 
 # Load cutsom stylesheet
 def setup(app) :
